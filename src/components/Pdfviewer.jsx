5ef8f0e69cc8dc8432590d0016e4d079ba1fdb79
Nhuttruong61 <Nhuttruong2261@gmail.com>
2023-04-13 17:43:45 +0700

    update

--- src/components/Pdfviewer.jsx
+++ src/components/Pdfviewer.jsx
@@ -1,15 +1,15 @@
-import { Document, Page } from 'react-pdf';
+import { Document, Page } from "react-pdf";
 
-import resumePDF from '../assets/cv/resume.pdf';
+import resumePDF from "../assets/cv/resume.pdf";
 
 function Pdfviewer() {
-return (
-<div>
-<Document file={resumePDF}>
-<Page pageNumber={1} />
-</Document>
-</div>
-);
+  return (
+    <div>
+      <Document file={resumePDF}>
+        <Page pageNumber={1} />
+      </Document>
+    </div>
+  );
 }
 
 export default Pdfviewer;
